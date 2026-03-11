73a26328dd54c1bad9a60abaebf1c0afebea1bfe
Olexxee <olexxee14@gmail.com>
2026-03-11 04:57:10 -0700

    Updated the cors and frontend Url

--- src/server.js
+++ src/server.js
@@ -5,14 +5,15 @@ import menuRouter from "./routes/menuRoutes.js";
 
 const app = express();
 
-// const allowedOrigins = [
-//   "http://localhost:3000",
-//   "http://localhost:5173",
-//   process.env.FRONTEND_URL,
-// ].filter(Boolean);
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:5173",
+  process.env.FRONTEND_URL,
+  "https://perfume-garden.vercel.app",
+].filter(Boolean);
 
 const corsOptions = {
-  origin: "*",
+  origin: allowedOrigins,
   credentials: true,
   methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
